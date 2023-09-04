093a8b10dd458e23680339030392ae9184fdb5f3
JonLoweFHSU <jdlowe@mail.fhsu.edu>
2023-09-04 18:24:30 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,37 @@
+let myName = "Ramsey"
+let myAge = 32;
+let myage = 23;
+
+myName 
+console.log(myname)
+
+document.getElementById("para").innerHTML = 
+"Hello all my name is" + myName + "And my name is" + myAge
+
+console.log("A" < "Z")
+
+document.getElementById("para").innerHTML = myname.charAt(2)
+document.getElementById("para").innerHTML = myname.charCodeAt(2)
+document.getElementById("para").innerHTML = myname.concat(myAge)
+console.log (myName.toUpperCase())
+console.log (myName.slice(1,2))
+
+//numbers
+
+const myNum = 27;
+const myFloat = 27.36;
+const myString = "25";
+
+console.log(myNum == myFloat)
+console.log(Number(myNum))
+
+//operations
+console.log(Math.PI)
+console.log(Math.trunc(Math.PI))
+console.log(Math.floor(Math.PI))
+console.log(Math.round(Math.PI))
+console.log(Math.ceil(Math.PI))
+
+console.log(Math.pow(2,4))
+console.log(Math.min(2,4,6,8))
+console.log(Math.max(2,4,6,8))
