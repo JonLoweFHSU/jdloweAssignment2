de21d813fea04ca355444916cba5f2a66f933eb8
JonLoweFHSU <143023039+JonLoweFHSU@users.noreply.github.com>
2023-09-18 12:35:25 -0500

    Delete week 4 directory

--- week 4/continue.js
+++
@@ -1,8 +0,0 @@
-for (let songNum = 1; songNum <=10; songNum++){
-    if(songNum ==5){
-        console.log('skipping song number' ${songNum})
-        continue;
-    }
-    console.log('playing song number' ${songNum})
-}
-console.log("Playlist is over")
